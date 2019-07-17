9d1194ca3608361b7cdc68d528a87a36f20f9d17
Supratik Chatterjee <supratikdevm96@gmail.com>
2019-07-17 16:47:27 +0530

    Create supratik.c

---
+++ supratik.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	long testcases, array_size, shift, k,l,m[100000],temp,n;
+	scanf("%d", &testcases); 
+	while(testcases-- > 0){
+		scanf("%d %d",&array_size, &shift);
+		for(l=0; l<array_size; l++) scanf("%d",&m[l]);
+		
+		shift  = shift % array_size;
+		
+		while(shift-- > 0){   
+			temp = m[i-1];
+			for(n=i-1;n > 0;) m[n] = m[--n];
+			m[0] = temp;
+		}
+		
+		while(i>0) printf("%d ",m[--i]);
+	}
+}
